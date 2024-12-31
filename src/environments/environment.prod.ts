8c1b0e3976cfef5f0cbaed99eba7efcc85efc88d
Frederic Onana <onanafrederic31@gmail.com>
2024-12-31 01:34:27 +0100

    add Bootstrap integration and Firebase environment configuration

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+
+  // src/environments/environment.prod.ts
+  export const environment = {
+    production: true,
+    firebaseConfig: {
+      apiKey: "AIzaSyC_ZLhTG4hHXghayTg8tbTsdWYWMCdIHuo",
+      authDomain: "practical3webdev.firebaseapp.com",
+      projectId: "practical3webdev",
+      storageBucket: "practical3webdev.firebasestorage.app",
+      messagingSenderId: "760988053848",
+      appId: "1:760988053848:web:eadf39d21b09db7fdcf52d",
+      measurementId: "G-V8ZBTQCQD9"
+    }
+  };
